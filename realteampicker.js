54f24e51e5ead8b01b728a6b7909c856df276c60
maprzybylski <mprzyb@outlook.com>
2019-05-27 11:30:19 +0200

    Added random functions

--- realteampicker.js
+++ realteampicker.js
@@ -1,1 +1,48 @@
- alert( 'Hello, world!' );
+var x=0;
+var array=new Array();
+var rand=0;
+var howManyTeams;
+var array2=new Array();
+
+function add_el_to_arr()
+{
+    array[x]=document.getElementById("text1").value;
+    x++;
+    document.getElementById("text1").value="";
+}
+
+function create_teams()
+{
+    howManyTeams=document.getElementById("text2").value;
+
+for(var z=0;z<howManyTeams;z++)
+{
+    array2[z]=new Array();
+}
+var c=0;
+for(var y=((array.length)/howManyTeams);y!=0;y--)
+{
+    for(var z=0;z<howManyTeams;z++)
+    {
+        rand= Math.floor((Math.random()*(array.length-1)));            
+        array2[z][c]=array[rand];
+        array.splice(rand,1);
+    }
+    c++;
+}
+display_arr();
+}
+
+function display_arr()
+{
+    var e="";
+    for(var y=0;y<array2.length;y++)
+    {
+        e+="Team "+(y+1)+"<br/>";
+        for(var z=0;z<array2[y].length;z++)
+        {
+            e+=array2[y][z]+"<br/>";
+        }
+    }
+    document.getElementById("Result").innerHTML=e;
+}
